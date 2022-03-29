a1766a8de0c1869723811c5bf2cbd10088aaff63
jonathantanuwijaya <jonathantanuwijaya0604@mhs.mdp.ac.id>
2022-03-29 15:51:16 +0700

    Add Dependency Injection

--- lib/infrastructure/auth/firebase_auth_facade.dart
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -2,10 +2,12 @@ import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
 import 'package:learning/domain/auth/auth_failure.dart';
 import 'package:learning/domain/auth/i_auth_facade.dart';
 import 'package:learning/domain/auth/value_object.dart';
 
+@Injectable(as: IAuthFacade)
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:learning/injection.dart';
 
 void main() {
+  configureInjection(Environment.prod);
   runApp(const MyApp());
 }
 
